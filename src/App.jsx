8da48bf8471b5f26d41e800d991afbd4aa8b8ff2
Yash Vishwakarma <yash2005vish@gmail.com>
2026-01-24 00:00:38 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Navbar from "./components/Navbar";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
